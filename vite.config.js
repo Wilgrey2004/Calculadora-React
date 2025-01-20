d9602546d9cc0cf49700dd8880bc27ba69ede6c1
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2025-01-19 22:14:44 -0400

    Agregando bien la url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://wilgrey2004.github.io/Calculadora-React.git",
+  base: "https://wilgrey2004.github.io/Calculadora-React/",
 });
